d9aef79426096c2a6772419910c4a491e9a4369a
reosfire <2.com9761721@gmail.com>
2024-12-08 12:40:11 +0300

    run single script function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,28 +1,27 @@
 import parsing.tokenize
-import runtime.ConstructorHandle
-import runtime.Memory
-import runtime.RunnableFunction
-import runtime.ThisHandle
+import runtime.*
 import treeBuilding.TreeNode
 import treeBuilding.buildTree
 import java.io.File
 
 fun main() {
-    val sourceCode = File("./examples/sorts.psc").readText()
+    runSingleScript("./examples/sorts.psc", "main", arrayOf())
+}
+
+fun runSingleScript(path: String, startFunction: String, args: Array<CallableClass>) {
+    val sourceCode = File(path).readText()
     val tokens = tokenize(sourceCode)
     println(tokens)
 
     val tree = buildTree(tokens)
     println(tree)
 
-    val functionsMap = tree.createFunctionsMap()
-
     val memory = Memory()
-    val thisHandle = ThisHandle(functionsMap)
+    val thisHandle = ThisHandle(tree.createFunctionsMap())
     memory.globalVariables["this"] = thisHandle
     memory.globalVariables["new"] = ConstructorHandle()
 
-    thisHandle.call("main", arrayOf(), memory)
+    thisHandle.call(startFunction, args, memory)
 }
 
 fun TreeNode.RootNode.createFunctionsMap(): Map<String, RunnableFunction> {
